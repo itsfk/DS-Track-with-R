39e10edd72b7f0c78fd716d8505d5208cc052ab0
itsfk <faiz.ggmu@gmail.com>
2019-05-19 16:37:49 +0500

    First Commit

---
+++ Introduction to R/Basic DataTypes R.R
@@ -0,0 +1,30 @@
+#Basic data types in R
+#R works with numerous data types. Some of the most basic types to get started are:Decimal values like 4.5 are called numerics.
+#Natural numbers like 4 are called integers. Integers are also numerics.
+#Boolean values (TRUE or FALSE) are called logical.
+#Text (or string) values are called characters.
+# Change my_numeric to be 42
+my_numeric <- 42
+
+# Change my_character to be "universe"
+my_character <- "universe"
+
+# Change my_logical to be FALSE
+my_logical <- FALSE
+
+#What's that data type? PART 2
+
+# Declare variables of different types
+my_numeric <- 42
+my_character <- "universe"
+my_logical <- FALSE 
+
+# Check class of my_numeric
+class(my_numeric)
+
+# Check class of my_character
+class(my_character)
+
+# Check class of my_logical
+class(my_logical)
+
---
+++ Introduction to R/Vectors.R
@@ -0,0 +1,35 @@
+# Create a vecor with C()
+numeric_vector <- c(1, 10, 49)
+character_vector <- c("a", "b", "c")
+
+# Complete the code for boolean_vector
+boolean_vector <- c(TRUE,FALSE,TRUE)
+
+#You can give a name to the elements of a vector with the names() function. 
+# Poker winnings from Monday to Friday
+poker_vector <- c(140, -50, 20, -120, 240)
+
+# Roulette winnings from Monday to Friday
+roulette_vector <- c(-24, -50, 100, -350, 10)
+
+# Assign days as names of poker_vector
+names(poker_vector) <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+
+# Assign days as names of roulette_vector
+names(roulette_vector) <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+
+##### NAMING A VECTOR #####
+# Poker winnings from Monday to Friday
+poker_vector <- c(140, -50, 20, -120, 240)
+
+# Roulette winnings from Monday to Friday
+roulette_vector <- c(-24, -50, 100, -350, 10)
+
+# The variable days_vector
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+
+# Assign the names of the day to roulette_vector and poker_vector
+names(poker_vector) <- days_vector  
+names(roulette_vector) <- days_vector 
+
+poker_vector
