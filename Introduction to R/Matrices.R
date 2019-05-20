80b59d4728a43cba0be3b0d215a3495b6879cb0a
itsfk <faiz.ggmu@gmail.com>
2019-05-20 20:37:40 +0500

    Matrices

---
+++ Introduction to R/Matrices.R
@@ -0,0 +1,112 @@
+#In R, a matrix is a collection of elements of 
+#the same data type (numeric, character, or logical) arranged into a fixed number of rows and columns.
+
+#In the matrix() function:
+  
+#The first argument is the collection of elements that R will arrange into the rows and columns 
+#of the matrix. Here, we use 1:9 which is a shortcut for c(1, 2, 3, 4, 5, 6, 7, 8, 9).
+#The argument byrow indicates that the matrix is filled by the rows. If we want the matrix to be
+#filled by the columns, we just place byrow = FALSE.
+#The third argument nrow indicates that the matrix should have three rows.
+
+#######  Analyze matrices, you shall######
+# Box office Star Wars (in millions!)
+new_hope <- c(460.998, 314.4)
+empire_strikes <- c(290.475, 247.900)
+return_jedi <- c(309.306, 165.8)
+
+# Create box_office
+box_office <- c(new_hope,empire_strikes,return_jedi)
+
+# Construct star_wars_matrix
+star_wars_matrix <- matrix(box_office,byrow= TRUE,nrow=3)
+
+###### Naming a matrix #########
+# Box office Star Wars (in millions!)
+new_hope <- c(460.998, 314.4)
+empire_strikes <- c(290.475, 247.900)
+return_jedi <- c(309.306, 165.8)
+
+# Construct matrix
+star_wars_matrix <- matrix(c(new_hope, empire_strikes, return_jedi), nrow = 3, byrow = TRUE)
+
+# Vectors region and titles, used for naming
+region <- c("US", "non-US")
+titles <- c("A New Hope", "The Empire Strikes Back", "Return of the Jedi")
+
+# Name the columns with region
+colnames(star_wars_matrix) <- region
+star_wars_matrix
+
+# Name the rows with titles
+rownames(star_wars_matrix) <- titles
+
+
+# Print out star_wars_matrix
+star_wars_matrix
+
+#### Calculating the worldwide box office #####
+#In R, the function rowSums() conveniently calculates the totals for each row of a matrix. 
+# This function creates a new vector:
+
+# Calculate worldwide box office figures
+worldwide_vector <- rowSums(star_wars_matrix)
+
+####### Adding a column for the Worldwide box office ######
+# Bind the new variable worldwide_vector as a column to star_wars_matrix
+all_wars_matrix <- cbind(star_wars_matrix,worldwide_vector)
+all_wars_matrix
+
+##### Adding a ROW #######
+all_wars_matrix <- rbind(star_wars_matrix,star_wars_matrix2)
+all_wars_matrix
+#### The total box office revenue for the entire saga ####
+# all_wars_matrix is available in your workspace
+all_wars_matrix
+
+# Total revenue for US and non-US
+total_revenue_vector <- colSums(all_wars_matrix)
+
+# Print out total_revenue_vector
+total_revenue_vector
+
+##### Selection of matrix elements #######
+# all_wars_matrix is available in your workspace
+all_wars_matrix
+
+# Select the non-US revenue for all movies
+non_us_all <- all_wars_matrix[,2]
+
+# Average non-US revenue
+mean(non_us_all)
+
+# Select the non-US revenue for first two movies
+non_us_some <- all_wars_matrix[1:2,2]
+non_us_some
+
+# Average non-US revenue for first two movies
+mean(non_us_some)
+
+######## A little arithmetic with matrices ######
+# all_wars_matrix is available in your workspace
+all_wars_matrix
+
+# Estimate the visitors
+visitors <- all_wars_matrix / 5
+
+# Print the estimate to the console
+visitors
+
+######## A little arithmetic with matrices 2 ######
+# all_wars_matrix and ticket_prices_matrix are available in your workspace
+all_wars_matrix
+ticket_prices_matrix
+
+# Estimated number of visitors
+visitors <- all_wars_matrix / ticket_prices_matrix
+
+# US visitors
+us_visitors <- visitors[,1]
+us_visitors
+# Average number of US visitors
+mean(us_visitors)
